d54518dde00f5d6988969153a01a6ab888205970
JIboJIb <66256996+JIboJIb@users.noreply.github.com>
2021-06-04 15:47:48 +0200

    Update Task2.py

--- Bool_if_else/Task2.py
+++ Bool_if_else/Task2.py
@@ -1,6 +1,8 @@
-program_name="vladyslav"
-name="Vladyslav"
-if program_name.lower()==name.lower():
- print("Good!")
-else:
-    print("try again")
+number = input("Write a number\n")
+while True:
+    if number.isdigit() and len(number) == 10:
+        print("Good")
+        break
+    else:
+        print("Try again")
+        number = input("Write a number with 10 digits and without leters\n")
