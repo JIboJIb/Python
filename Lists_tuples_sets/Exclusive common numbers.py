519bb9ae6e63a6de82cc502753a1eca24c1e4b60
JIboJIb <66256996+JIboJIb@users.noreply.github.com>
2021-06-07 23:51:51 +0200

    Add files via upload

--- Lists_tuples_sets/Exclusive common numbers.py
+++ Lists_tuples_sets/Exclusive common numbers.py
@@ -3,7 +3,7 @@
 import random
 list1 = []
 list2 = []
-listIdentical = []
+list_identical = []
 i = 0
 while i < 10:
     list1.append(random.randrange(1, 10))
@@ -11,10 +11,10 @@ while i < 10:
     i += 1
 print(list1)
 print(list2)
-list1Modified = set(list1)
-list2Modified = set(list2)
+list1_modified = set(list1)
+list2_modified = set(list2)
 # Перетворюєм список у множини щоб знайти одинакові числа без повторів set()
-listIdentical = list(list1Modified & list2Modified)
+list_identical = list(list1_modified & list2_modified)
 # & для пошуку спільних об'єктів https://www.programiz.com/python-programming/set
 # list() перетворюємо в список
-print(listIdentical)
+print(list_identical)
