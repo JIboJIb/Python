af0147becc3a60c132619b116c0f2c78649b28e6
JIboJIb <66256996+JIboJIb@users.noreply.github.com>
2021-08-08 21:48:54 +0200

    Add files via upload

---
+++ Modules_and_st_libraries/Imports practice/Task1.py
@@ -0,0 +1,7 @@
+# Make a directory with 2 modules; make a function in one of them; then import this function in the other module and
+# use that in your script of choice.
+import func_concatenation
+func_concatenation.concatenation(5, 3)
+
+
+print(func_concatenation.concatenation(5, 3))
---
+++ Modules_and_st_libraries/Imports practice/func_concatenation.py
@@ -0,0 +1,4 @@
+# Make a directory with 2 modules; make a function in one of them; then import this function in the other module and
+# use that in your script of choice.
+def concatenation(a, b):
+    return a + b
