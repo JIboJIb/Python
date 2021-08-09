a1e80fb73b46974053a30764a4ae08ccbdc73fca
JIboJIb <66256996+JIboJIb@users.noreply.github.com>
2021-08-09 22:27:38 +0200

    Add files via upload

---
+++ Modules_and_st_libraries/The sys module.py
@@ -0,0 +1,7 @@
+# The “sys.path” list is initialized from the PYTHONPATH environment variable. Is it possible to change it from within
+# Python? If so, does it affect where Python looks for module files? Run some interactive tests to find it out.
+import sys
+print(sys.path[2])
+a = sys.path
+a.remove(sys.path[2])
+print(sys.path[2])
