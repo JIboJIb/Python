7f540a7397f462e83dc673d96e961197053714ca
JIboJIb <66256996+JIboJIb@users.noreply.github.com>
2021-08-11 23:43:27 +0200

    Update Mymod.py

--- Modules_and_st_libraries/Mymod.py
+++ Modules_and_st_libraries/Mymod.py
@@ -1,8 +1,19 @@
 def count_lines(name):
-    with open(name, 'r') as i:
+    with open(name, 'r', encoding='utf-8') as i:
         my_lines = i.readlines()
     return len(my_lines)
 
 
-if __name__ == '__main__':
-    print(count_lines('Mike Inez.txt'))
+def count_chars(name):
+    with open(name, 'r', encoding='utf-8') as i:
+        my_chars = i.read()
+    return len(my_chars)
+
+
+def test(name):
+    return f"Lines: {count_lines(name)} chars {count_chars(name)}"
+
+
+print(count_lines('Mike Inez.txt'))
+print(count_chars('Mike Inez.txt'))
+print(test('Mike Inez.txt'))
