0361632439c8c61a3bde34cbbb68f7e9b4f4e302
Hladkyi Vlad <66256996+JIboJIb@users.noreply.github.com>
2021-09-14 19:59:20 +0200

    Update Task3.py

--- Stacks_queues/Task3.py
+++ Stacks_queues/Task3.py
@@ -20,8 +20,48 @@ class Stack:
         else:
             return True
 
-    def get_from_stack(self, stack, item):
-        if item in stack:
+     def get_from_stack(self, item):
+        if item in self.stack:
             return item
         else:
-            raise ValueError
+            raise ValueError(f'Element "{item}" did not search.')
+
+
+stack = Stack()
+for i in range(10):
+    stack.push(i)
+print(stack.get_from_stack(8))
+print(stack.get_from_stack(10))
+
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, item):
+        self.queue.insert(0, item)
+
+    def dequeue(self):
+        return self.queue.pop()
+
+    def isEmpty(self):
+        if self.queue:
+            return True
+        else:
+            return False
+
+    def peek(self):
+        return self.queue[-1]
+
+    def get_from_queue(self, item):
+        if item in self.queue:
+            return item
+        else:
+            raise ValueError(f'Element "{item}" did not search.')
+
+
+q = Queue()
+for char in 'honey':
+    q.enqueue(char)
+print(q.get_from_queue('h'))
+print(q.get_from_queue('a'))
